22e650636cf56986be5e615297a9317b525ac746
wabrocker <billbrocker@gmail.com>
2025-09-19 09:09:23 -0400

    Implement ESLint & Prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Acquisitions");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Acquisitions');
 });
 
 export default app;
